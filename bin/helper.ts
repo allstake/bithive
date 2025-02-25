a74b1807cb9067c09fd7d206d26a2dd04a8e5d6e
link2web3 <173345699+link2web3@users.noreply.github.com>
2025-02-25 11:02:33 +0800

    chore: testnet3 (#44)

--- bin/helper.ts
+++ bin/helper.ts
@@ -2,7 +2,7 @@ import { tGas } from "near-workspaces";
 import readline from "readline";
 
 export const envBuilder = {
-  choices: ["dev", "stg", "prod", "testnet4", "signet"],
+  choices: ["stg", "prod", "testnet3", "testnet4", "signet"],
   describe: "Environment name",
   demandOption: true,
 };
